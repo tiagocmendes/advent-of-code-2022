2415d28d021d18b6368fa31dcdbb09c2a8de9842
Tiago Mendes <tiago.mendes@talkdesk.com>
2022-12-05 23:08:56 +0000

    Day 1: Calorie Counting - Part Two

--- day-01/solution.js
+++ day-01/solution.js
@@ -21,23 +21,47 @@ function parseCaloriesRecords(data) {
     return caloriesByElf;
 }
 
-function findElfWithMostCalories(data) {
+function computeTotalCaloriesByElf(data) {
     const caloriesByElf = parseCaloriesRecords(data);
-    const totalCaloriesByElf = caloriesByElf.map((calories) =>
+
+    return caloriesByElf.map((calories) =>
         calories.reduce((acc, value) => acc + value, 0)
     );
+}
+
+function findElfWithMostCalories(data) {
+    let totalCaloriesByElf = computeTotalCaloriesByElf(data);
 
     return Math.max(...totalCaloriesByElf);
 }
 
-fs.readFile("input.txt", "utf8", (err, data) => {
-    if (err) {
-        console.error(err);
-        return;
+function findTopThreeElvesWithMostCalories(data) {
+    let totalCaloriesByElf = computeTotalCaloriesByElf(data);
+
+    let totalCalories = 0;
+    for (let tier = 0; tier < 3; tier++) {
+        const maxCalories = Math.max(...totalCaloriesByElf);
+        totalCalories += maxCalories;
+        totalCaloriesByElf = totalCaloriesByElf.filter(
+            (calories) => calories != maxCalories
+        );
     }
+    return totalCalories;
+}
+
+function main() {
+    const data = fs.readFileSync("input.txt", "utf8").toString();
 
     const elfWithMostCalories = findElfWithMostCalories(data);
+    const topThreeElvesWithMostCalories =
+        findTopThreeElvesWithMostCalories(data);
+
     console.log(
         `How many total Calories is that Elf carrying? Answer: ${elfWithMostCalories}`
     );
-});
+    console.log(
+        `How many Calories are those Elves carrying in total? ${topThreeElvesWithMostCalories}`
+    );
+}
+
+main();
